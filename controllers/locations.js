150f5984f1f7527e2a0c3104ab8be86894e81cf2
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2019-11-20 17:16:11 -0500

    locations.js controllercreated. Have not tested functionality.

---
+++ controllers/locations.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const Locations = require('../models/locations.js');
+
+//===============
+//=== Show All Locations
+//===============
+router.get('/', (req,res) => {
+    Locations.find({},(err, allLocations) => {
+        res.json(allLocations);
+    });
+});
+
+//===============
+//=== Create New Location
+//===============
+router.post('/', (req,res) => {
+    Locations.create(req.body, (error, newLocation) => {
+        res.json(newLocation);
+    });
+});
+
+//===============
+//=== Edit A Location
+//===============
+router.put('/:id', (req,res) => {
+    Locations.findByIdAndUpdate(req.params.id, req.body, (err, updatedLocation) => {
+        res.json(updatedLocation);
+    });
+});
+
+//===============
+//=== Delete A Location
+//===============
+router.delete('/:id', (req, res) => {
+    Locations.findByIdAndDelete(req.params.id, (err, deletedLocation) => {
+        res.json(deletedLocation);
+    });
+});
+
+module.exports = router;

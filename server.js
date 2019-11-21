620e85ee50e1e34d2ec3da30c8369a049e3bec10
Troy Hancock <Troy@Troys-MacBook-Pro.local>
2019-11-21 05:33:03 -0500

    test

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 5000
-require('dotenv').config();
+// require('dotenv').config();
+if (app.get('env') == 'development'){ require('dotenv').config(); }
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 // const host = process.env.CLUSTER

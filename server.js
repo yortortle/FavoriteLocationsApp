4c3764765635ed34b2c7c41d9ce616ad82b2a2f5
Troy Hancock <Troy@Troys-MacBook-Pro.local>
2019-11-21 05:55:21 -0500

    asf

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const router = express.Router();
-const port = process.env.PORT || 5000
+// const PORT = process.env.PORT;
 require('dotenv').config();
 // if (app.get('env') == 'development'){ require('dotenv').config(); }
 const mongoose = require('mongoose');
@@ -64,4 +64,4 @@ db.on( 'open' , ()=>{
 /////////////////////
 //Listener
 /////////////////////
-app.listen(port, () => console.log(`listening on ${port}!`))
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
